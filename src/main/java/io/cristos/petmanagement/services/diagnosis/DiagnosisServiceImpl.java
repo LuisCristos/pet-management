fb05b3d4347cbfb6b3cdcdb0a2afbd0ab8f5d931
Quinas <info@la-prison.de>
2023-02-04 13:17:39 +0100

    added: improvement

--- src/main/java/io/cristos/petmanagement/services/diagnosis/DiagnosisServiceImpl.java
+++ src/main/java/io/cristos/petmanagement/services/diagnosis/DiagnosisServiceImpl.java
@@ -11,12 +11,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class DiagnosisServiceImpl implements DiagnosisService {
 
     private final Logger logger = LoggerFactory.getLogger(DiagnosisServiceImpl.class);
