e7f2d74818d28bfda9fb0b5b0f745b5a4e05d729
Quinas <info@la-prison.de>
2023-02-05 12:10:23 +0100

    added: validation on controller and implementation

--- src/main/java/io/cristos/petmanagement/controllers/contact/ContactController.java
+++ src/main/java/io/cristos/petmanagement/controllers/contact/ContactController.java
@@ -2,19 +2,33 @@ package io.cristos.petmanagement.controllers.contact;
 
 
 import io.cristos.petmanagement.dtos.contact.ContactDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface ContactController {
 
-    ResponseEntity<ContactDto> saveContact(ContactDto contactDto);
+    ResponseEntity<ContactDto> saveContact(@Valid
+                                           @RequestBody ContactDto contactDto);
 
     ResponseEntity<List<ContactDto>> getAllContacts();
 
-    ResponseEntity<ContactDto> findContactById(Long contactId);
+    ResponseEntity<ContactDto> findContactById(@PathVariable(name = "contactId")
+                                               @NotNull
+                                               @Min(1) Long contactId);
 
-    ResponseEntity<ContactDto> deleteContactById(Long contactId);
+    ResponseEntity<ContactDto> deleteContactById(@PathVariable(name = "contactId")
+                                                 @NotNull
+                                                 @Min(1) Long contactId);
 
-    ResponseEntity<ContactDto> updateContactById(Long contactId, ContactDto contactDto);
+    ResponseEntity<ContactDto> updateContactById(@PathVariable(name = "contactId")
+                                                 @NotNull
+                                                 @Min(1) Long contactId,
+                                                 @Valid
+                                                 @RequestBody ContactDto contactDto);
 }
--- src/main/java/io/cristos/petmanagement/controllers/contact/ContactControllerImpl.java
+++ src/main/java/io/cristos/petmanagement/controllers/contact/ContactControllerImpl.java
@@ -4,11 +4,14 @@ import io.cristos.petmanagement.dtos.contact.ContactDto;
 import io.cristos.petmanagement.models.contact.Contact;
 import io.cristos.petmanagement.services.contact.ContactService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -17,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/contacts")
+@Validated
 public class ContactControllerImpl implements ContactController {
 
     private final Logger logger = LoggerFactory.getLogger(ContactControllerImpl.class);
@@ -29,7 +33,8 @@ public class ContactControllerImpl implements ContactController {
 
     @Override
     @PostMapping
-    public ResponseEntity<ContactDto> saveContact(@Valid @RequestBody ContactDto contactDto) {
+    public ResponseEntity<ContactDto> saveContact(@Valid
+                                                  @RequestBody ContactDto contactDto) {
 
         Contact contact = contactService.saveContact(contactDto);
 
@@ -55,7 +60,9 @@ public class ContactControllerImpl implements ContactController {
 
     @Override
     @GetMapping("/{contactId}")
-    public ResponseEntity<ContactDto> findContactById(@PathVariable Long contactId) {
+    public ResponseEntity<ContactDto> findContactById(@PathVariable(name = "contactId")
+                                                      @NotNull
+                                                      @Min(1) Long contactId) {
 
         logger.info("Find contact with id: " + contactId);
 
@@ -64,7 +71,9 @@ public class ContactControllerImpl implements ContactController {
 
     @Override
     @DeleteMapping("/{contactId}")
-    public ResponseEntity<ContactDto> deleteContactById(@PathVariable final Long contactId) {
+    public ResponseEntity<ContactDto> deleteContactById(@PathVariable(name = "contactId")
+                                                        @NotNull
+                                                        @Min(1) Long contactId) {
 
         contactService.deleteContactById(contactId);
 
@@ -75,8 +84,11 @@ public class ContactControllerImpl implements ContactController {
 
     @Override
     @PutMapping("/{contactId}")
-    public ResponseEntity<ContactDto> updateContactById(@PathVariable Long contactId,
-                                                        @Valid @RequestBody ContactDto contactDto) {
+    public ResponseEntity<ContactDto> updateContactById(@PathVariable(name = "contactId")
+                                                        @NotNull
+                                                        @Min(1) Long contactId,
+                                                        @Valid
+                                                        @RequestBody ContactDto contactDto) {
         contactService.updateContact(contactId, contactDto);
 
         logger.info("Updated Contact with contactId: " + contactId);
